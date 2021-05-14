f8cf1e0ea5dba54ebbb5d09d310e622048eda176
Kancherla Kishorereddy <49707819+kancherlakishorereddy@users.noreply.github.com>
2021-05-14 23:12:56 +0530

    cors update

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.set('view engine', 'ejs');
 app.use(express.static('./views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(router);
 
 app.listen(PORT);
